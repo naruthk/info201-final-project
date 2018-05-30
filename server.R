39f97384d0ea1e29d4fbf890ff812c9b7eab956f
Emma Sommerseth <esomm97@uw.edu>
2018-05-30 16:51:08 -0700

    merge conflict resolved

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 national_arrests <- read.csv("data/arrests_national.csv")
 
 state <- read.csv("data/estimated_crimes.csv")
@@ -8,17 +8,6 @@ library(dplyr)
 library(tidyr)
 
 my_server <- function(input, output) {
-=======
-
-  national_arrests <- read.csv("data/arrests_national.csv")
-  
-  state <- read.csv("data/estimated_crimes.csv")
-  library(plotly)
-  library(ggplot2)
-  library(dplyr)
-  library(tidyr)
-  
-  my_server <- function(input, output) {
     
     
     national_arrests <- national_arrests %>%
@@ -63,7 +52,7 @@ my_server <- function(input, output) {
     
     
   }
->>>>>>> emma
+
   
 
     
--- ui.R
+++ ui.R
@@ -1,4 +1,3 @@
-
 library(ggplot2)
 library(dplyr)
 library(shiny)
@@ -18,27 +17,25 @@ my_ui <- fluidPage(
                   tabPanel("Plot", plotOutput("plot3"),
                            selectInput("select_year", h3("Select Year"), choices = list("1995" = 1995, "1996" = 1996, "1997" = 1997,
                                                                                         "1998" = 1998, "1999" = 1999, 
-<<<<<<< HEAD
-                                                                                         "2000" = 2000,"2001" = 2001,"2002" = 2002,
-=======
-                                                                                        "2000" = 2000,"2001" = 2001,"2002" = 2002,
->>>>>>> emma
+                                                                                       
+                                                                                       
+                                                                                      
+                                                                                          "2000" = 2000,"2001" = 2001,"2002" = 2002,
+                                                                                       
                                                                                         "2003" = 2003, "2004"= 2004,"2005" = 2005,
                                                                                         "2006" = 2006,"2007" = 2007, "2008" = 2008,
                                                                                         "2009" = 2009,"2010" = 2010,"2011" = 2011,
                                                                                         "2012" = 2012, "2013" = 2013,"2014" = 2014,
                                                                                         "2015" = 2015,"2016" = 2016
                                                                                         ,selected = 2000)),
-<<<<<<< HEAD
-                                        dataTableOutput("table"), textOutput("plot_info")), #mine
-=======
-                           dataTableOutput("table"), textOutput("plot_info")), #mine
->>>>>>> emma
-                  tabPanel("Plot", plotOutput("plot4"))
-                  
+                        
+             
+                    dataTableOutput("table"), textOutput("plot_info")), #mine
+      
+      tabPanel("Plot", plotOutput("plot4"))
+      
     )
   )
   
 )
 )
-shinyUI(my_ui)
